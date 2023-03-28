22d2d7ecd6fb0d212795ecee557670bf0f174836
PearcePunisher <skully1333@gmail.com>
2023-03-28 17:49:12 -0600

    1 file modified

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -46,19 +46,19 @@ function createData(
 }
 
 const rows = [
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Donut', 452, 25.0, 51, 4.9),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Honeycomb', 408, 3.2, 87, 6.5),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Jelly Bean', 375, 0.0, 94, 0.0),
-  createData('KitKat', 518, 26.0, 65, 7.0),
-  createData('Lollipop', 392, 0.2, 98, 0.0),
-  createData('Marshmallow', 318, 0, 81, 2.0),
-  createData('Nougat', 360, 19.0, 9, 37.0),
-  createData('Oreo', 437, 18.0, 63, 4.0),
+  createData('107.9', 305, 3.7, 67, 4.3),
+  createData('94.3', 452, 25.0, 51, 4.9),
+  createData('103.9', 262, 16.0, 24, 6.0),
+  createData('95.1', 159, 6.0, 24, 4.0),
+  createData('98.9', 356, 16.0, 49, 3.9),
+  createData('100.7', 408, 3.2, 87, 6.5),
+  createData('105.9', 237, 9.0, 37, 4.3),
+  createData('92.5', 375, 0.0, 94, 0.0),
+  createData('92.9', 518, 26.0, 65, 7.0),
+  createData('88.7', 392, 0.2, 98, 0.0),
+  createData('98.5', 318, 0, 81, 2.0),
+  createData('89.3', 360, 19.0, 9, 37.0),
+  createData('88.1', 437, 18.0, 63, 4.0),
 ];
 
 function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
@@ -116,31 +116,31 @@ const headCells: readonly HeadCell[] = [
     id: 'name',
     numeric: false,
     disablePadding: true,
-    label: 'Dessert (100g serving)',
+    label: 'Station call number',
   },
   {
     id: 'calories',
     numeric: true,
     disablePadding: false,
-    label: 'Calories',
+    label: 'Colorado Springs',
   },
   {
     id: 'fat',
     numeric: true,
     disablePadding: false,
-    label: 'Fat (g)',
+    label: 'Ft. Collins',
   },
   {
     id: 'carbs',
     numeric: true,
     disablePadding: false,
-    label: 'Carbs (g)',
+    label: 'North Denver',
   },
   {
     id: 'protein',
     numeric: true,
     disablePadding: false,
-    label: 'Protein (g)',
+    label: 'Castle Rock',
   },
 ];
 
