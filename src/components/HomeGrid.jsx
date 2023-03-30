f6c254d7da53008fef95da7a79167c973647af78
PearcePunisher <skully1333@gmail.com>
2023-03-30 13:51:46 -0600

    Creating and Modifying various components

--- src/components/HomeGrid.jsx
+++ src/components/HomeGrid.jsx
@@ -3,6 +3,26 @@ import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Unstable_Grid2";
+import Table from "../components/Table.jsx";
+import ListTest from "../components/ListTest.jsx"
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#e32421',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+    background: {
+      default: '#272934',
+      paper: '#1f2129',
+    },
+  },
+});
+
 
 const Item = styled(Paper)(({ theme }) => ({
 	backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -18,11 +38,11 @@ export default function BasicGrid() {
 			<Grid
 				container
 				spacing={2}>
-				<Grid xs={8}>
-					<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio praesentium voluptate fugiat itaque beatae rem quod odio labore, sequi odit iste expedita id harum temporibus sunt ex eligendi modi quis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, accusantium blanditiis. Officia voluptatum possimus maxime laborum omnis ipsum necessitatibus, enim iste at culpa veniam molestias inventore libero rem odio quas!</p>
-				</Grid>
 				<Grid xs={4}>
-					<Item>xs=4</Item>
+					<ListTest />
+				</Grid>
+				<Grid xs={8}>
+					<Table />
 				</Grid>
 			</Grid>
 		</Box>
